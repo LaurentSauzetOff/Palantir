496336e2699b06ea108c2a44125f9bd46c07d9de
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2026-04-26 13:52:44 +0200

    feat(ui): add full shadcn/ui component set

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="font-bold text-3xl text-indigo-500">Coucou !</p>
+    <div className="flex flex-col">
+      <p className="font-bold text-3xl text-indigo-500">Coucou !</p>
+    </div>
   );
 }
